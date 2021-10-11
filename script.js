529d28aec07c3e11e3aa6229a2684d2f874595e2
Abhay Sharma <55479887+abhi302005@users.noreply.github.com>
2021-10-11 16:22:59 +0530

    change to js and css
    add hidden class
    add random number and display the dice

--- script.js
+++ script.js
@@ -12,10 +12,24 @@ const score1El = document.getElementById('score--1');
 const current1El = document.getElementById('current--1');
 
 // dice role
-const diceE1 = document.querySelector('.dice');
+const dice = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('btn--hold');
 
 // meta info.
 let scores, currentScore, activePlayer, playing;
+
+// start conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+dice.classList.add('hidden');
+
+btnRoll.addEventListener('click', function() {
+    // generating random number
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+
+    // display the dice
+    dice.classList.remove('hidden');
+    dice.src = `dice-${randomNumber}.png`;
+});
